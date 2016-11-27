922b4f5353eb494686dca0b4708e71a5c374803b
Soundararajan <soundararajan@usa.net>
2016-11-26 22:47:27 -0800

    Todo initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hasgHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
 
 ReactDOM.render(
-    <h1>Boilerplate app</h1>,
+    <TodoApp />,
     document.getElementById("app")
 );
