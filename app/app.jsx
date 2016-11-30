312d08c0e38d00f228fb9dd64244d4c6a37bdd83
Soundararajan <soundararajan@usa.net>
2016-11-29 16:10:24 -0800

    Add redux  store

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hasgHistory} = require('react-router');
+
+
+
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 var TodoApp = require('TodoApp');
+var actions = require('./actions/actions');
+var store = require('configureStore').configure();
+
+store.subscribe( () => {
+   console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo("Learn redux with react"));
+store.dispatch(actions.setSearchText("redux"));
+store.dispatch(actions.toggleShowCompleted());
 
 ReactDOM.render(
     <TodoApp />,
